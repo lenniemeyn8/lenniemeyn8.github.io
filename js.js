0ac8d25b9131009e009aeccddf8d3ec1e7bc87fd
lenniemeyn8 <lmeyn@unomaha.edu>
2018-05-19 19:30:35 -0500

    queryselectorchange

--- js.js
+++ js.js
@@ -15,7 +15,7 @@ function addNewPosts(){
 
     //listOfContainers = document.getElementsByClassName("_5pcr userContentWrapper");
     //listOfPosterName = document.querySelectorAll('div.userContentWrapper:not(._4nef)');
-    listOfPosterName = document.querySelectorAll('div._5pcr userContentWrapper');
+    listOfPosterName = document.querySelectorAll('_5pcr userContentWrapper');
     
     //each container holds a post. For each container the name and more has to displayed
     for (var count=0; count < listOfContainers.length; count++){
